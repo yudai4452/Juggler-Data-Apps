ce77a0049bedc25c4bcefb710cd234ca18138b42
yudai4452 <yudaiprogram@gmail.com>
2024-10-22 22:55:22 +0900

    Update juggler_data_visualizer.py

--- juggler_data_visualizer.py
+++ juggler_data_visualizer.py
@@ -8,6 +8,10 @@ def load_excel_data(excel_path):
     df = pd.read_excel(excel_path, sheet_name="合成確率", engine="openpyxl", index_col=0)
     return df
 
+# 分数表示のための関数
+def format_as_fraction(value):
+    return f"1/{int(value)}" if value != 0 else "N/A"
+
 # グラフのプロット
 def plot_synthetic_probabilities(df, selected_machine_number):
     machine_data = df.loc[selected_machine_number].dropna()
@@ -16,6 +20,9 @@ def plot_synthetic_probabilities(df, selected_machine_number):
     
     # 1/合成確率を計算
     inverse_probabilities = 1 / probabilities
+
+    # 軸ラベルを分数形式に変換
+    fraction_labels = [format_as_fraction(p) for p in probabilities]
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=inverse_probabilities, mode='lines+markers', name=f'1/合成確率: {selected_machine_number}'))
@@ -26,6 +33,8 @@ def plot_synthetic_probabilities(df, selected_machine_number):
         xaxis=dict(tickformat="%Y-%m-%d"),
         hovermode="x"
     )
+    # Y軸のラベルを分数形式に変更
+    fig.update_yaxes(tickvals=inverse_probabilities, ticktext=fraction_labels)
     st.plotly_chart(fig)
 
 # Streamlitアプリケーションのインターフェース
